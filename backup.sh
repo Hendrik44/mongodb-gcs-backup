a3fbc06bb20062e1539911784db2bb5ff7d87c2e
Hendrik Jürst <Hendrik44@users.noreply.github.com>
2020-05-13 21:49:47 +0200

    Fixed and improved backup script

--- backup.sh
+++ backup.sh
@@ -22,6 +22,8 @@ SLACK_CHANNEL=${SLACK_CHANNEL:-}
 SLACK_USERNAME=${SLACK_USERNAME:-}
 SLACK_ICON=${SLACK_ICON:-}
 
+gsutil -v
+
 backup() {
   mkdir -p $BACKUP_DIR
   date=$(date "+%Y-%m-%dT%H:%M:%SZ")
@@ -66,7 +68,9 @@ default_api_version = 2
 EOF
   fi
   echo "uploading backup archive to GCS bucket=$GCS_BUCKET"
-  gsutil cp $BACKUP_DIR/$archive_name $GCS_BUCKET
+  gsutil cp $BACKUP_DIR/$archive_name gs://$GCS_BUCKET/$archive_name
+  echo "verify that transfered file exist in bucket because google is saying ok but sometimes there is nothing..."
+  gsutil stat gs://$GCS_BUCKET/$archive_name
 }
 
 send_slack_message() {
